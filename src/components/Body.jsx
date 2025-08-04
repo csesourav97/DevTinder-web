203557377592c128d70a71afa8d3a6fb2796654e
Sourav Ghosh <cse.souravghosh@gmail.com>
2025-08-04 08:54:23 +0530

    redirect user to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,6 +14,7 @@ const Body = () => {
   const userData = useSelector((store) => store.user);
 
   const fetchUser = async () => {
+    if (userData) return;
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
@@ -29,9 +30,7 @@ const Body = () => {
   };
 
   useEffect(() => {
-    if (!userData) {
-      fetchUser();
-    }
+    fetchUser();
   }, []);
 
   return (
